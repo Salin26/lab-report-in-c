03f4c7644998f088f531936000e0d4b40cedc281
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-08-14 17:36:18 +0600

    Create grade_calculator.c

---
+++ grade_calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter score: ");
+    scanf("%d", &n);
+
+    if (n > 80) {
+        printf("A+\n");
+    } 
+    else if (n >= 75 && n <= 80) {
+        printf("A\n");
+    } 
+    else if (n >= 70 && n <= 74) {
+        printf("A-\n");
+    } 
+    else if (n >= 65 && n <= 69) {
+        printf("B+\n");
+    } 
+    else if (n >= 60 && n <= 64) {
+        printf("B\n");
+    } 
+    else if (n >= 55 && n <= 59) {
+        printf("B-\n");
+    } 
+    else if (n >= 50 && n <= 54) {
+        printf("C+\n");
+    } 
+    else if (n >= 45 && n <= 49) {
+        printf("C\n");
+    } 
+    else if (n >= 40 && n <= 44) {
+        printf("D\n");
+    } 
+    else {
+        printf("F\n");
+    }
+
+    return 0;
+}
